1ea2450b9b5de330d4f03fa3803b21660d7e2358
Ajinkya Rathod <57681532+ajinzrathod@users.noreply.github.com>
2020-06-21 03:38:47 +0530

    First Commit from PC

---
+++ any-content-zoombox-edit/gallery.php
@@ -0,0 +1,17 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link href="css/zoombox.css" rel="stylesheet">
+<style>
+  .gallery-thumb { height: 225px; width: 300px; padding: 10px;}
+</style>
+
+<a class="zoombox zgallery1" title="This is an image" href="img-gallery/1.jpg"><img src="img-gallery/1.jpg" class="gallery-thumb" /></a>
+<a class="zoombox zgallery1" title="This is an image" href="img-gallery/2.jpg"><img src="img-gallery/2.jpg" class="gallery-thumb" /></a>
+<a class="zoombox zgallery1" title="This is an image" href="img-gallery/3.jpg"><img src="img-gallery/3.jpg" class="gallery-thumb" /></a>
+
+<script src="js/jquery-1.12.4.js"></script>
+<script src="js/zoombox.js"></script>
+
+<script>
+  $('a.zoombox').zoombox();
+</script>
